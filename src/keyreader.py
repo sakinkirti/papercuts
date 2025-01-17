00918e8983065782c3f4869fe2d6e726e347bb0f
sakinkirti <sakin.arya.kirti@gmail.com>
2025-01-17 13:21:24 -0500

    updating to working prototype

--- src/keyreader.py
+++ src/keyreader.py
@@ -3,7 +3,8 @@ import yaml
 class KeyReader:
     
     def __init__(self, keys:str):
-        self.keys = yaml.safe_load(keys)
+        with open(keys, 'r') as stream:
+            self.keys = yaml.safe_load(stream)
         
     def get(self, key:str):
         return self.keys[key]
